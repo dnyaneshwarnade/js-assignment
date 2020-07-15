b45f979a83f4fda0603aee70f2a29f1b748dcdb5
dnyaneshwarnade <68270134+dnyaneshwarnade@users.noreply.github.com>
2020-07-15 12:59:13 +0530

    Add files via upload

---
+++ consolelog.js
@@ -0,0 +1,18 @@
+
+console.log("Hello My name is Sachin Ghatavane")
+console.log("I am learning the javascript with Lets Upgrade")
+
+console.error("an error is occured ! !")
+
+console.warn("I am giving a warning to you be in your limits dont cross the line ")
+
+console.log("Time Taken:")
+
+var emp1={Name:"Sachin" ,age:"30", Salary:"40000"}
+var emp2={Name:"Shubham", age:"25", Salary:"20000"}
+var emp3={Name:"Sumit", age:"28", Salary:"30000"}
+var emp4={Name:"Sahil", age:"40", Salary:"45000"}
+
+console.table({emp1, emp2, emp3, emp4})
+
+console.timeEnd("Time Taken:")
